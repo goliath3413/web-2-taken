74dc2477a787cfc6316ec2c3c4aa067ea971af83
George Tirnovan <geo.tirnovan@gmail.com>
2022-01-20 16:32:42 +0100

    1js

---
+++ main/name-popup/script.js
@@ -0,0 +1,7 @@
+var naam = prompt("WHATS YO NAME")
+alert('Who asked tho')
+
+var color = prompt("gib meh colur");
+var body = document.getElementsByTagName("h1")[0];
+console.log(el);
+el.style.backgroundColor = color;
