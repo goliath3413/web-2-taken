b0a97de4252f38cf2e6a81f6bc6e7ae6fb20d15a
George Tirnovan <geo.tirnovan@gmail.com>
2022-01-25 15:24:57 +0100

    stap 3 jscript

--- main/name-popup/script.js
+++ main/name-popup/script.js
@@ -1,7 +1,10 @@
-var naam = prompt("WHATS YO NAME")
-alert('Nobody asked lol')
 
-var color = prompt("gib meh colur");
+var color = prompt("gib meh colur for body");
 var body = document.getElementsByTagName("body")[0];
 console.log(body);
 body.style.backgroundColor = color;
+
+var color = prompt("gib meh colur for text");
+var h1 = document.getElementsByTagName("h1")[0];
+console.log(h1);
+h1.style.color = color;
