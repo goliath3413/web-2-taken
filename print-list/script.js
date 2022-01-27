6ee72936d2d186634c08eb314bcaa9add2f7f942
George Tirnovan <geo.tirnovan@gmail.com>
2022-01-27 16:41:22 +0100

    list unfinished

---
+++ print-list/script.js
@@ -0,0 +1,18 @@
+
+
+var singer1 = {
+    firstName : "Jole-Richard",
+    lastName : "Hughes",
+    genre : "Frenchcore",
+    
+}
+var singer2 = {
+    firstName : "sds",
+    lastName : "fsf",
+    genre : "fsfs",
+    
+}
+var List = [singer1, singer2];
+
+for (let singer1 of List)
+console.log(List)
