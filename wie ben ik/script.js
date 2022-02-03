f9e3c5fb59f7722e14c3c93c24a58a930281abb1
George Tirnovan <geo.tirnovan@gmail.com>
2022-02-03 16:35:40 +0100

    not exactly right wie ben ik

--- wie ben ik/script.js
+++ wie ben ik/script.js
@@ -1,8 +1,19 @@
-'use strict';
+"use strict";
 console.log("George's thing works ;))))");
-let h1Element = document.querySelector('.h1');
+let h1Element = document.querySelector(".h1");
 console.log(h1Element);
-h1Element.style.backgroundColor = 'red';
-var element = document.getElementById("George").style.textDecoration = "underline";
-var element2 = document.getElementById("Achraf").style.backgroundColor = "Red"
-var element3 = document.getElementById("Adrien").style.backgroundColor = "Red"
+h1Element.style.backgroundColor = "red";
+// var names = ["Achraf", "Adrien"];(big help from Maxim)
+var element = document.getElementById("George");
+element.style.textDecoration ="underline";
+
+var element2 = document.querySelectorAll(".vijfde-rij");
+
+for (var i = 0; i < element2.length; i++) {
+  element2[i].style.backgroundColor = "red";
+}
+
+// class names
+// let cash1 = document.getElementsByClassName(cash2);
+//for(let cash1 of cash2) {element}
+// cash1.style.backgroundcolor = 'red;
