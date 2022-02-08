bf1d1514dbbe1ef67240d9069e0d4d2a9dc5c6b4
George Tirnovan <geo.tirnovan@gmail.com>
2022-02-08 14:09:38 +0100

    1st step of math

---
+++ wiskunde/script.js
@@ -0,0 +1,9 @@
+// Alert= browser cmd promt. ();. slash instead of numpad divided works
+// let + create name = to number lets you make math out of words. (let num1 = 10; let num2 = 20; let sum = num1 + num2; alert(sum);
+// prompting to ask
+// variable with *1 to normalise then do the math.
+let num1 = prompt('give num 1');
+let num2 = prompt('give num 2');
+let sum = (num1*1) + (num2*1);
+alert(sum);
+
