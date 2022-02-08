acab503b27f2ebae76d135dfe22a784bd43cf2f1
George Tirnovan <geo.tirnovan@gmail.com>
2022-02-08 16:06:47 +0100

    math 3

--- wiskunde/script.js
+++ wiskunde/script.js
@@ -2,8 +2,17 @@
 // let + create name = to number lets you make math out of words. (let num1 = 10; let num2 = 20; let sum = num1 + num2; alert(sum);
 // prompting to ask
 // variable with *1 to normalise then do the math.
-let num1 = prompt('give num 1');
-let num2 = prompt('give num 2');
-let sum = (num1*1) + (num2*1);
-alert(sum);
 
+let num1 = prompt("give number 1");
+let num2 = prompt("give number 2");
+let num1_num2 = num1 > num2;
+
+  if (num1_num2) {
+    alert("number 1 is bigger than number 2");
+  } else num2 > num1;
+  {
+    alert("number 2 is bigger than number1");
+  }
+
+
+  
